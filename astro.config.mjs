319561791e6ed8f2a1ebf4be0b870c829893c806
highseas1851 <patrick@tpmm.nyc>
2026-01-02 16:46:23 -0500

    suggest to fix this config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,3 +3,11 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({});
+
+// astro.config.mjs
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  // Set this to 'ignore' so both /about and /about/ work locally
+  trailingSlash: 'ignore' 
+});
